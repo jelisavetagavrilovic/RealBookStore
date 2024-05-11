788e64e29a109e928b678c51639a854e72635576
jelisavetagavrilovic <jekagavrilovic@gmail.com>
2024-05-11 18:53:09 +0200

    csrf defense

--- src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/PersonsController.java
@@ -5,6 +5,7 @@ import com.urosdragojevic.realbookstore.domain.Person;
 import com.urosdragojevic.realbookstore.domain.User;
 import com.urosdragojevic.realbookstore.repository.PersonRepository;
 import com.urosdragojevic.realbookstore.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.nio.file.AccessDeniedException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -52,7 +54,13 @@ public class PersonsController {
     }
 
     @PostMapping("/update-person")
-    public String updatePerson(Person person) {
+    // public String updatePerson(Person person) {
+    public String updatePerson(Person person, HttpSession session,
+                               @RequestParam("csrfToken") String csrfToken) throws AccessDeniedException {
+                                    String csrf = session.getAttribute("CSRF_TOKEN").toString();
+                                    if (!csrf.equalsIgnoreCase(csrfToken)) {
+                                        throw new AccessDeniedException("Access Denied");
+                                    }
         personRepository.update(person);
         return "redirect:/persons/" + person.getId();
     }
